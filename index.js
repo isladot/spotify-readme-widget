f893386711edac2765e95c09f5aabae86c865fd9
Luca Biagetti <notciabaku@gmail.com>
2021-06-05 23:14:43 +0200

    chore: heroku stuffs 2.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 
 app.listen(process.env.PORT || 3000, process.env.HOST || '::', (err) => {
   if (err) throw err;
-  console.log(`Server listening on ${app.server.address().port}`);
+  console.log(`Server started.`);
 });
